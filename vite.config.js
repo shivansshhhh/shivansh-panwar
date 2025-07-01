16b9d28bdebbb6b1af3245940e525d2cf8d76e43
Shivansh Panwar <167331111+shivansshhhh@users.noreply.github.com>
2025-07-01 16:54:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/https://github.com/shivansshhhh/shivansshhhh.github.io/',
+  base: '/',
   plugins: [react()],
 });
 
